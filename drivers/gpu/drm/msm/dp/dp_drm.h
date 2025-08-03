784933bd7b1ce82c51a1f16006248ee6eeba2473
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2025-08-03 14:53:51 +0300

    BACKPORT: drm/bridge: add connector argument to .hpd_notify callback
    Drivers might need to update DRM connector in the
    drm_bridge_funcs.hpd_notify callback (e.g. it might be necessary to
    update EDID before setting ELD). Add corresponding argument to the
    callback.
    
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
    Link: https://patch.msgid.link/20250803-lt9611uxc-hdmi-v1-1-cb9ce1793acf@oss.qualcomm.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/gpu/drm/msm/dp/dp_drm.h
+++ drivers/gpu/drm/msm/dp/dp_drm.h
@@ -30,6 +30,7 @@ enum drm_connector_status msm_dp_bridge_detect(struct drm_bridge *bridge,
 void msm_dp_bridge_hpd_enable(struct drm_bridge *bridge);
 void msm_dp_bridge_hpd_disable(struct drm_bridge *bridge);
 void msm_dp_bridge_hpd_notify(struct drm_bridge *bridge,
-			  enum drm_connector_status status);
+			      struct drm_connector *connector,
+			      enum drm_connector_status status);
 
 #endif /* _DP_DRM_H_ */
