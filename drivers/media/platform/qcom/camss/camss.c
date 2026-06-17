c457d7a9ec5dc214ef736b276b309a6461193e86
David Heidelberg <david@ixit.cz>
2026-06-17 10:32:20 +0200

    FROMLIST: media: qcom: camss: csiphy: Introduce PHY configuration
    Read PHY configuration from the device-tree bus-type and save it into
    the csiphy structure for later use.
    
    For C-PHY, skip clock line configuration, as there is none.
    
    Link: https://lore.kernel.org/r/20260617-qcom-cphy-v9-2-83da8a8e4e44@ixit.cz
    Acked-by: Cory Keitz <ckeitz@amazon.com>
    Reviewed-by: Bryan O'Donoghue <bod@kernel.org>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: David Heidelberg <david@ixit.cz>
    Signed-off-by: Anusha Arun Nandi <anusha.nandi@oss.qualcomm.com>

--- drivers/media/platform/qcom/camss/camss.c
+++ drivers/media/platform/qcom/camss/camss.c
@@ -5566,9 +5566,13 @@ static int camss_parse_endpoint_node(struct device *dev,
 	csd->interface.csiphy_id = vep.base.port;
 
 	mipi_csi2 = &vep.bus.mipi_csi2;
-	lncfg->clk.pos = mipi_csi2->clock_lane;
-	lncfg->clk.pol = mipi_csi2->lane_polarities[0];
 	lncfg->num_data = mipi_csi2->num_data_lanes;
+	lncfg->phy_cfg = vep.bus_type;
+
+	if (lncfg->phy_cfg != V4L2_MBUS_CSI2_CPHY) {
+		lncfg->clk.pos = mipi_csi2->clock_lane;
+		lncfg->clk.pol = mipi_csi2->lane_polarities[0];
+	}
 
 	lncfg->data = devm_kcalloc(dev,
 				   lncfg->num_data, sizeof(*lncfg->data),
