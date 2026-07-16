80f51744df483eb2f7cd41b8edf20be0d8710107
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-07-16 14:56:34 +0530

    Revert "FROMLIST: media: iris: Introduce set_preset_register as a vpu_op"
    This reverts commit d4c2c8c900b8b3f96d620935843d3d0fbb28c200.
    
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_vpu3x.c
+++ drivers/media/platform/qcom/iris/iris_vpu3x.c
@@ -261,7 +261,6 @@ const struct vpu_ops iris_vpu3_ops = {
 	.power_on_controller = iris_vpu_power_on_controller,
 	.calc_freq = iris_vpu3x_vpu4x_calculate_frequency,
 	.set_hwmode = iris_vpu_set_hwmode,
-	.set_preset_registers = iris_vpu_set_preset_registers,
 };
 
 const struct vpu_ops iris_vpu33_ops = {
@@ -271,7 +270,6 @@ const struct vpu_ops iris_vpu33_ops = {
 	.power_on_controller = iris_vpu_power_on_controller,
 	.calc_freq = iris_vpu3x_vpu4x_calculate_frequency,
 	.set_hwmode = iris_vpu_set_hwmode,
-	.set_preset_registers = iris_vpu_set_preset_registers,
 };
 
 const struct vpu_ops iris_vpu35_ops = {
@@ -282,5 +280,4 @@ const struct vpu_ops iris_vpu35_ops = {
 	.program_bootup_registers = iris_vpu35_vpu4x_program_bootup_registers,
 	.calc_freq = iris_vpu3x_vpu4x_calculate_frequency,
 	.set_hwmode = iris_vpu_set_hwmode,
-	.set_preset_registers = iris_vpu_set_preset_registers,
 };
