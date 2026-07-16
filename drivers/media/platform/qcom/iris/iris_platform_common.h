404878ab5e2c0494a6f089678b174e0f64eec660
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-07-16 14:56:34 +0530

    Revert "FROMLIST: media: iris: Add Gen2 firmware autodetect and fallback"
    This reverts commit ee2b755196cdd5d8cb518516a8155d1a756c71e6.
    
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_platform_common.h
+++ drivers/media/platform/qcom/iris/iris_platform_common.h
@@ -289,7 +289,11 @@ struct iris_firmware_desc {
 };
 
 struct iris_platform_data {
-	const struct iris_firmware_desc *firmware_desc_gen1, *firmware_desc_gen2;
+	/*
+	 * XXX: replace with gen1 / gen2 pointers once we have platforms
+	 * supporting both firmware kinds.
+	 */
+	const struct iris_firmware_desc *firmware_desc;
 
 	const struct vpu_ops *vpu_ops;
 	const struct icc_info *icc_tbl;
