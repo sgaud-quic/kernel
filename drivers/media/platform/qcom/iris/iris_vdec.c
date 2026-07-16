166c0e642c3276d76bf1d8e7108c022121aee039
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-07-16 14:56:34 +0530

    Revert "FROMLIST: media: iris: Filter UBWC raw formats based on hardware capabilities"
    This reverts commit e4c043f6445276e92e3b8c14aef4de8e2edacb60.
    
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_vdec.c
+++ drivers/media/platform/qcom/iris/iris_vdec.c
@@ -3,7 +3,6 @@
  * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
-#include <linux/soc/qcom/ubwc.h>
 #include <media/v4l2-event.h>
 #include <media/v4l2-mem2mem.h>
 
@@ -72,7 +71,6 @@ static const u32 iris_vdec_formats_cap[] = {
 
 static bool check_format(struct iris_inst *inst, u32 pixfmt, u32 type)
 {
-	const struct qcom_ubwc_cfg_data *ubwc = inst->core->ubwc_cfg;
 	unsigned int size, i;
 	const u32 *fmt;
 
@@ -84,9 +82,6 @@ static bool check_format(struct iris_inst *inst, u32 pixfmt, u32 type)
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE:
 		fmt = iris_vdec_formats_cap;
 		size = ARRAY_SIZE(iris_vdec_formats_cap);
-		/* Last format is UBWC; drop it if UBWC is unsupported */
-		if (!ubwc->ubwc_enc_version)
-			size--;
 		break;
 	default:
 		return false;
@@ -115,7 +110,6 @@ static bool check_format(struct iris_inst *inst, u32 pixfmt, u32 type)
 
 static u32 find_format_by_index(struct iris_inst *inst, u32 index, u32 type)
 {
-	const struct qcom_ubwc_cfg_data *ubwc = inst->core->ubwc_cfg;
 	unsigned int size;
 	const u32 *fmt;
 
@@ -127,9 +121,6 @@ static u32 find_format_by_index(struct iris_inst *inst, u32 index, u32 type)
 	case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE:
 		fmt = iris_vdec_formats_cap;
 		size = ARRAY_SIZE(iris_vdec_formats_cap);
-		/* Last format is UBWC; drop it if UBWC is unsupported */
-		if (!ubwc->ubwc_enc_version)
-			size--;
 		break;
 	default:
 		return 0;
