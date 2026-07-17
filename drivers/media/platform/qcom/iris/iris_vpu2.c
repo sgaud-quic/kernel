3598e7c52e580860521af880476e77d2d0cf3939
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-07-17 11:59:19 +0530

    PENDING: media: iris: add firmware sub-device support for vpu2
    A dedicated firmware device is required to map the firmware region to
    the device's IOMMU mapped stream ID, which is the firmware stream ID,
    in order to boot the Iris firmware when no hypervisor is present.
    
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_vpu2.c
+++ drivers/media/platform/qcom/iris/iris_vpu2.c
@@ -8,9 +8,29 @@
 #include <linux/reset.h>
 
 #include "iris_instance.h"
+#include "iris_resources.h"
 #include "iris_vpu_common.h"
 
 #include "iris_vpu_register_defines.h"
+
+static int iris_vpu2_init_cb_devs(struct iris_core *core)
+{
+	struct device *dev;
+
+	dev = iris_create_cb_dev(core, "video-firmware");
+	if (IS_ERR(dev))
+		return PTR_ERR(dev);
+
+	core->fw_dev = dev;
+
+	return 0;
+}
+
+static void iris_vpu2_deinit_cb_devs(struct iris_core *core)
+{
+	if (core->fw_dev)
+		platform_device_unregister(to_platform_device(core->fw_dev));
+}
 
 const struct vpu_ops iris_vpu2_ops = {
 	.power_off_hw = iris_vpu_power_off_hw,
@@ -21,4 +41,6 @@ const struct vpu_ops iris_vpu2_ops = {
 	.set_hwmode = iris_vpu_set_hwmode,
 	.set_preset_registers = iris_vpu_set_preset_registers,
 	.interrupt_init = iris_vpu_interrupt_init,
+	.init_cb_devs = iris_vpu2_init_cb_devs,
+	.deinit_cb_devs = iris_vpu2_deinit_cb_devs,
 };
