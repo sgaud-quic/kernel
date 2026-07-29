495c26ac21184f73f233e6e32908d80c79d82637
Imran Shaik <imran.shaik@oss.qualcomm.com>
2026-07-29 20:53:47 +0530

    FROMGIT: clk: qcom: Fix test_ctl_hi field for DEFAULT_EVO PLLs
    CLK_ALPHA_PLL_TYPE_DEFAULT_EVO type PLLs do not have the PLL_TEST_CTL_U1
    register, so clk_alpha_pll_configure() does not program test_ctl_hi1_val
    for this PLL type.
    
    The GCC PLL configurations for QCM2290, Shikra and SM6115 wrongly use
    test_ctl_hi1_val instead of test_ctl_hi_val, deviating from the hardware
    recommended settings. Fix them to use test_ctl_hi_val.
    
    Fixes: 496d1a13d405 ("clk: qcom: Add Global Clock Controller driver for QCM2290")
    Fixes: 01cf3e27824d ("clk: qcom: Add Global clock controller support on Qualcomm Shikra SoC")
    Fixes: e88c533d8a2a ("clk: qcom: gcc-sm6115: Add missing PLL config properties")
    Cc: stable@vger.kernel.org
    Signed-off-by: Imran Shaik <imran.shaik@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Link: https://lore.kernel.org/all/20260729-pll-test-ctrl-fixup-v1-1-246d79589380@oss.qualcomm.com/

--- drivers/clk/qcom/gcc-shikra.c
+++ drivers/clk/qcom/gcc-shikra.c
@@ -123,7 +123,7 @@ static const struct alpha_pll_config gpll10_config = {
 	.vco_mask = GENMASK(21, 20),
 	.main_output_mask = BIT(0),
 	.config_ctl_val = 0x4001055b,
-	.test_ctl_hi1_val = 0x1,
+	.test_ctl_hi_val = 0x1,
 };
 
 static struct clk_alpha_pll gpll10 = {
@@ -156,7 +156,7 @@ static const struct alpha_pll_config gpll11_config = {
 	.vco_mask = GENMASK(21, 20),
 	.main_output_mask = BIT(0),
 	.config_ctl_val = 0x4001055b,
-	.test_ctl_hi1_val = 0x1,
+	.test_ctl_hi_val = 0x1,
 };
 
 static struct clk_alpha_pll gpll11 = {
@@ -361,7 +361,7 @@ static const struct alpha_pll_config gpll8_config = {
 	.post_div_val = BIT(8),
 	.post_div_mask = GENMASK(11, 8),
 	.config_ctl_val = 0x4001055b,
-	.test_ctl_hi1_val = 0x1,
+	.test_ctl_hi_val = 0x1,
 };
 
 static struct clk_alpha_pll gpll8 = {
