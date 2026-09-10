9fb4aee2470a80e4504ad9c8b6d29941b7f84c6f
Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>
2026-09-10 15:16:54 +0530

    FROMLIST: ASoC: qcom: lpass-macro-common: Add LPASS codec version 4.1
    Shikra integrates LPASS codec version 4.1. Add the new version to the
    codec version enum and to the version string helper.
    
    The enum stays ordered by version so that the existing '>=' comparisons
    on enum lpass_codec_version keep working.
    
    Link: https://lore.kernel.org/all/20260910-add_shikra_support-v2-3-fdd2129c696c@oss.qualcomm.com/
    Signed-off-by: Mohammad Rafi Shaik <mohammad.rafi.shaik@oss.qualcomm.com>

--- sound/soc/codecs/lpass-macro-common.h
+++ sound/soc/codecs/lpass-macro-common.h
@@ -30,6 +30,7 @@ enum lpass_codec_version {
 	LPASS_CODEC_VERSION_2_7,
 	LPASS_CODEC_VERSION_2_8,
 	LPASS_CODEC_VERSION_2_9,
+	LPASS_CODEC_VERSION_4_1,
 };
 
 struct lpass_macro {
@@ -70,6 +71,8 @@ static inline const char *lpass_macro_get_codec_version_string(int version)
 		return "v2.8";
 	case LPASS_CODEC_VERSION_2_9:
 		return "v2.9";
+	case LPASS_CODEC_VERSION_4_1:
+		return "v4.1";
 	default:
 		break;
 	}
